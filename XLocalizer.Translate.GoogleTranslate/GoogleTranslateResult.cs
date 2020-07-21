be5022fee2d5a4976d04726e991662642058b3dc
LazZiya <ziyadmm@gmail.com>
2020-07-21 13:17:27 +0300

    Add project files.

---
+++ XLocalizer.Translate.GoogleTranslate/GoogleTranslateResult.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace XLocalizer.Translate.GoogleTranslate
+{
+    /// <summary>
+    /// Google translate result object
+    /// </summary>
+    public class GoogleTranslateResult
+    {
+        /// <summary>
+        /// Google trnaslate data object
+        /// </summary>
+        [JsonProperty("data")]
+        public GoogleData Data { get; set; }
+    }
+
+    /// <summary>
+    /// Google translate data object
+    /// </summary>
+    public class GoogleData
+    {
+        /// <summary>
+        /// Google translate result list
+        /// </summary>
+        [JsonProperty("translations")]
+        public GoogleTranslation[] Translations { get; set; }
+    }
+
+    /// <summary>
+    /// Google tranlste result text
+    /// </summary>
+    public class GoogleTranslation
+    {
+        /// <summary>
+        /// Google translate result text
+        /// </summary>
+        [JsonProperty("translatedText")]
+        public string TranslatedText { get; set; }
+    }
+}
---
+++ XLocalizer.Translate.GoogleTranslate/README.txt
@@ -0,0 +1,21 @@
+﻿XLocalizer.TranslationServices.GoogleTranslate
+
+Instructions to use this package :
+
+- This package requires Rapid API Key, must be obtained from https://rapidapi.com/googlecloud/api/google-translate1
+- Add the API key to user secrets :
+
+````
+{
+  "XLocalizer.TranslationServices": {
+    "RapidApiKey": "xxx-rapid-api-key-xxx"
+  }
+}
+````
+
+- Register in startup:
+````
+services.AddHttpClient<ITranslationService, GoogleTranslateService>();
+````
+
+Repository: https://github.com/LazZiya/TranslationServices
